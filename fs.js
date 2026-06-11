2e5b44cfc2a33b9f63748cfc81c5baf41fa03cf0
12steve <gcbismark@gmail.com>
2026-06-11 01:22:04 -0500

    Second Commit

--- fs.js
+++ fs.js
@@ -1,1 +1,29 @@
 // fs.js
+const FS = {
+    drives: {
+        "C:": {
+            name: "C:",
+            volumeLabeL: "LocalMachine",
+            type: "dir",
+            children: {
+                "WINDOWS": {
+                    name: "Windows",
+                    type: "dir",
+                    creationTime: Date.now(),
+                    lastModified: Date.now(),
+                    attrs: 0b0000,
+                    children: {
+                        "DESKTOP": {
+                            name: "Desktop",
+                            type: "dir",
+                            creationTime: Date.now(),
+                            lastModified: Date.now(),
+                            attrs: 0b0000,
+                            children: {}
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
